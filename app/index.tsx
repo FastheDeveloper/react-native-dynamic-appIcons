1921ab19141237d3922bb5dd6981be458d461a9b
Farouq Seriki <farouq@basaltx.io>
2024-09-18 16:48:55 +0100

    IconSetup

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Stack, Link } from 'expo-router';
-
+import { changeIcon, getIcon, resetIcon } from 'react-native-change-icon';
 import { Button } from '~/components/Button';
 import { Container } from '~/components/Container';
 import { ScreenContent } from '~/components/ScreenContent';
@@ -10,9 +10,13 @@ export default function Home() {
       <Stack.Screen options={{ title: 'Home' }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
+
+        <Button
+          onPress={() => {
+            changeIcon('Gutarist');
+          }}
+          title="Show Details"
+        />
       </Container>
     </>
   );
